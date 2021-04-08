6c7989ace94b30019a45c5ff483af858e9f2365e
dangcap99992 <nguyentienloc18122000@gmail.com>
2021-04-08 19:56:01 +0700

    Crawl

---
+++ main.py
@@ -0,0 +1,46 @@
+
+import  bs4;
+import  requests;
+import pymongo;
+Description=[];
+myclient=pymongo.MongoClient("mongodb://localhost:27017/");
+mydb=myclient["Findingjob"];
+mycol=mydb["Job"]
+url='https://itviec.com/viec-lam-it';
+def getpagecontent(url):
+     page=requests.get(url,headers={"Accept-Language":"en-US"});
+     return bs4.BeautifulSoup(page.text,"html.parser")
+soup=getpagecontent(url);
+divtags=soup.findAll("div",class_="job_content")
+for divtag in divtags:
+      NameJob=divtag.find("h2",class_="title").text;
+      city=divtag.find("div",class_="city").text;
+      NameJob.replace('\r\n', ' $ ');
+      city.replace("\n","");
+      Skills=[];
+      Skill=divtag.find("div",class_="job-bottom").find("div",class_="tag-list").findAll("a",class_="mkt-track")
+      for skills in Skill:
+          Skills.append(skills.find("span").text.replace("\n",""))
+      PathURL="https://itviec.com/"+divtag.find("h2",class_="title").find("a")["href"];
+      soup1=getpagecontent(PathURL);
+      divtagssoup1=soup1.find("div",class_="job-details");
+      SVGIcon=divtagssoup1.find("div",class_="job-details__overview").findAll("div",class_="svg-icon")
+      for svgicon in SVGIcon:
+          for svgicon1 in svgicon.findAll("div",class_="svg-icon__text"):
+              if(svgicon1.find("span")!=None):
+                Location=svgicon1.find("span").text;
+      Location.replace("\n","");
+      Time=divtagssoup1.find("div",class_="job-details__overview").find("div",class_="svg-icon--blue")
+      ReallyTime=Time.find("div",class_="svg-icon__text").text
+      ReallyTime.replace("\n","");
+      Jobdetails=divtagssoup1.findAll("div",class_="job-details__paragraph")
+      DescriptionJob=str(Jobdetails[0].find("p"))
+      RequirementJob=str(Jobdetails[1].find("p"))
+      mydick={"NameJob":NameJob,
+            "JobDescription":DescriptionJob,
+             "JobRequirements":RequirementJob,
+            "Location":Location,
+            "Skills":Skills,
+            "Time":ReallyTime,
+            "City":city}
+      mycol.insert_one(mydick)
