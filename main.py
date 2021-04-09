1f269d0e850dfc90eee9f29485bd264c57b79199
dangcap99992 <nguyentienloc18122000@gmail.com>
2021-04-09 20:38:15 +0700

    Update Topdev crawl data

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 
-<<<<<<< HEAD
 import  bs4
 import  requests
 import pymongo
 myclient=pymongo.MongoClient("mongodb://localhost:27017/")
 mydb=myclient["Findingjob"]
-=======
-import  bs4;
-import  requests;
-import pymongo;
-Description=[];
-myclient=pymongo.MongoClient("mongodb://localhost:27017/");
-mydb=myclient["FindingJob"];
->>>>>>> bf44d8663ef0c46bfd09058f3dc7326416e7fc70
 mycol=mydb["Job"]
 url='https://itviec.com/viec-lam-it'
 urltopdev="https://topdev.vn/viec-lam-it"
 def getpagecontent(url):
      page=requests.get(url,headers={"Accept-Language":"en-US"})
      return bs4.BeautifulSoup(page.text,"html.parser")
-<<<<<<< HEAD
 # def craw_Itwork():
 #     soup=getpagecontent(url);
 #     divtags=soup.findAll("div",class_="job_content")
@@ -103,37 +93,6 @@ def crawl_topdev():
             "NameJob":NameJob,
             "JobDescription":JobDescription,
             "JobRequirements":JobRequirement,
-=======
-soup=getpagecontent(url);
-divtags=soup.findAll("div",class_="job_content")
-for divtag in divtags:
-      NameJob=divtag.find("h2",class_="title").text
-      city=divtag.find("div",class_="city").text.strip()
-      print(city)
-      NameJob.replace('\r\n', ' $ ')
-      Skills=[]
-      Skill=divtag.find("div",class_="job-bottom").find("div",class_="tag-list").findAll("a",class_="mkt-track")
-      for skills in Skill:
-          Skills.append(skills.find("span").text.replace("\n",""))
-      PathURL="https://itviec.com/"+divtag.find("h2",class_="title").find("a")["href"];
-      soup1=getpagecontent(PathURL);
-      divtagssoup1=soup1.find("div",class_="job-details");
-      SVGIcon=divtagssoup1.find("div",class_="job-details__overview").findAll("div",class_="svg-icon")
-      for svgicon in SVGIcon:
-          for svgicon1 in svgicon.findAll("div",class_="svg-icon__text"):
-              if(svgicon1.find("span")!=None):
-                Location=svgicon1.find("span").text;
-      Location.replace("\n","");
-      Time=divtagssoup1.find("div",class_="job-details__overview").find("div",class_="svg-icon--blue")
-      ReallyTime=Time.find("div",class_="svg-icon__text").text.strip()
-      print(ReallyTime)
-      Jobdetails=divtagssoup1.findAll("div",class_="job-details__paragraph")
-      DescriptionJob = str(Jobdetails[0].find("p"))
-      RequirementJob = str(Jobdetails[1].find("p"))
-      mydick={"NameJob":NameJob,
-            "JobDescription":DescriptionJob,
-             "JobRequirements":RequirementJob,
->>>>>>> bf44d8663ef0c46bfd09058f3dc7326416e7fc70
             "Location":Location,
             "YearOfExperiene":YearOfExperiene,
             "JobType":JobType,
